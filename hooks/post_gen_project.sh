97e5067d243ca23143e3b6aa94600d7f9ee47634
Emile Fraser <efraser25@gmail.com>
2022-09-27 21:54:16 +0200

    fixed post_gen_hook

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,6 +3,7 @@ cd {{ cookiecutter.project_slug }}
 rm -rf .git
 
 git remote add origin git@github.com:{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}
+git remote -v
 git add *
 git commit -m "cookiecutter base template"
-git push
+git push --set-upstream origin main
