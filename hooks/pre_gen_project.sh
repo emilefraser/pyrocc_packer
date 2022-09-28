fa744a90128b9892d4675577f61f26caab5cf962
Emile Fraser <efraser25@gmail.com>
2022-09-29 00:26:22 +0200

    hooks changes

--- hooks/pre_gen_project.sh
+++ hooks/pre_gen_project.sh
@@ -2,10 +2,11 @@
 
 # github check
 REPO_URL=git@github.com:emilefraser/{{ cookiecutter.project_slug }}
+echo "REPO_URL to check: ${REPO_URL}"
 
 [ "$(git ls-remote $REPO_URL 2> /dev/null)" ] && {
-	echo Repo Exists;
+	echo "${REPO_URL} already exists;"
 } || {
-	echo Repo does NOT exist;
+	echo "${REPO_URL} does NOT exist;
     gh repo create {{ cookiecutter.project_slug }} --public
 }
