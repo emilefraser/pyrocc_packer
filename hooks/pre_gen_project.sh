4fdb499dea9bdf47c15948f02ff0d5673829e092
Emile Fraser <efraser25@gmail.com>
2022-09-29 00:46:37 +0200

    changed pre hook if then else statement

--- hooks/pre_gen_project.sh
+++ hooks/pre_gen_project.sh
@@ -4,9 +4,10 @@
 REPO_URL=git@github.com:emilefraser/{{ cookiecutter.project_slug }}
 echo "REPO_URL to check: ${REPO_URL}"
 
-[ "$(git ls-remote $REPO_URL 2> /dev/null)" ] && {
-	echo "${REPO_URL} already exists;"
-} || {
-	echo "${REPO_URL} does NOT exist;
+if [ "$(git ls-remote $REPO_URL 2> /dev/null)" ]
+then
+	echo "${REPO_URL} already exists"
+else
+	echo "${REPO_URL} does NOT exist"
     gh repo create {{ cookiecutter.project_slug }} --public
-}
+fi
