1a0715253ca6438655fe7e87ce200ea0ffc21286
Emile Fraser <efraser25@gmail.com>
2022-09-29 11:36:29 +0200

    pre hook finally corrected

--- hooks/pre_gen_project.sh
+++ hooks/pre_gen_project.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 # github check
-REPO_URL=https://github.com/emilefraser/{{ cookiecutter.project_slug }}
-echo "REPO_URL to check: ${REPO_URL}"
+REPO_GH=git@github.com
+REPO_USER={{ cookiecutter.github_username }}
+REPO_NAME={{ cookiecutter.project_slug }}
+REPO_URL=${REPO_GH}:${REPO_USER}/${REPO_NAME}
+REPO_LICENSE="MIT"
+REPO_TYPE="Packer"
+
+echo "Checking if repo: ${REPO_NAME} exists at url: ${REPO_URL}"
+
+[ "$(git ls-remote $REPO_URL 2> /dev/null)" ]
 
 if [ "$(git ls-remote $REPO_URL 2> /dev/null)" ]
 then
-	echo "${REPO_URL} already exists"
+	echo "${REPO_NAME} already exists"
 else
-	echo "${REPO_URL} does NOT exist"
-    gh repo create {{ cookiecutter.project_slug }} --public
+	echo "${REPO_NAME} does NOT exist"
+	gh repo --public --license ${REPO_LICENCE} --gitignore ${REPO_TYPE} ${REPO_NAME}
 fi
